cd2e325ad9e2b2e1ad4ac3b051572376bdced22c
Romain Caddoux <romain.caddoux@gmail.com>
2016-03-07 17:00:07 +0100

    initial commit, add plugin.xml + www

---
+++ www/bistri-js-api.js
@@ -0,0 +1,46 @@
+( function ()
+{
+
+    function LazyJSLoader ( source, callback )
+    {
+        this._loaded   = 0,
+        this._source  = source instanceof Array ? source : [ source ],
+        this._callback = callback;
+        this._load ();
+    };
+
+    LazyJSLoader.prototype.load = function ()
+    {
+        this._source.forEach ( function ( src )
+        {
+            var _script = document.createElement ( 'script' );
+
+            window.document.body.appendChild ( _script );
+
+            if ( typeof this._callback == 'function' )
+            {
+                _script.addEventListener ( 'load', function ()
+                {
+                    if ( ++this._loaded == this._source.length )
+                    {
+                        this._callback ();
+                    }
+                }.bind ( this ) );
+            }
+            _script.src = src;
+        }.bind ( this ) );
+    };
+
+    window.onBistriConferenceReady = function ()
+    {
+        console.log ( "bc", bc );
+    };
+
+    var loader = new LazyJSLoader ();
+
+    loader.load ( 'https://api.bistri.com/bistri.conference.min.js', function ()
+    {
+        console.log ( "bistri.conference.min.js loaded" );
+    } );
+
+} ) ();
